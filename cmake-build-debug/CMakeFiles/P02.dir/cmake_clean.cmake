30e22830ee98949f3182de175057de9ff595fb11
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2019-10-22 10:43:05 -0700

    initial project

---
+++ cmake-build-debug/CMakeFiles/P02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P02.dir/main.cpp.obj"
+  "P02.exe"
+  "P02.exe.manifest"
+  "P02.pdb"
+  "libP02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/P02.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for P02.
+# This may be replaced when dependencies are built.
